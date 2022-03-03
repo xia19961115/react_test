adfe179ddd226ea25b49180e7cd6a17603434223
夏宁 <478084629@qq.com>
2022-03-03 20:11:43 +0800

    1

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -3,7 +3,7 @@
  * @Auther: xianing
  * @LastEditors: xianing
  * @Date: 2022-03-03 10:49:27
- * @LastEditTime: 2022-03-03 19:10:12
+ * @LastEditTime: 2022-03-03 20:10:20
  */
 import React from 'react'
 import SilderBar from '@/components/SilderBar'
@@ -13,6 +13,7 @@ export default function Main() {
   const navigate = useNavigate()
   // 进入权限路由先获取用户权限ID
   const roleList = localStorage.getItem('roleList')
+  console.log('=========',roleList)
   // 没权限直接进登录页
   if (!roleList) navigate('/login')
   // 获取动态路由列表
@@ -21,11 +22,9 @@ export default function Main() {
   // 无列表 直接跳登录
   if (Base.length === 0 ) {
     navigate('/login')
-    return
   }
   // 获取所有页面的路径
   const arr = Base.map(item => '/main/'+item.path )
-  console.log(arr)
   return (
     <div>
       <SilderBar roleRoute={Base} rolePath={arr}/>
