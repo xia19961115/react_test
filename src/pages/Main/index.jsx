b181e6e286a2518aa2b14082dda7dcc7cd696edd
夏宁 <478084629@qq.com>
2022-03-04 02:04:35 +0800

    11

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -13,21 +13,23 @@ export default function Main() {
   const navigate = useNavigate();
   // 进入权限路由先获取用户权限ID
   const roleList = localStorage.getItem("roleList");
-  console.log("=========", roleList);
   // 没权限直接进登录页
   if (!roleList) navigate("/login");
   // 获取动态路由列表
-  console.log(routes);
   let Base = routes.filter((item) => roleList?.includes(item.roleId) || item.roleId === '*');
   // 无列表 直接跳登录
-  if (Base.length === 0) {
-    navigate("/login");
-  }
-  // Base.push(welcome)
-  console.log(Base);
+  // if (Base.length === 1) {
+  //   navigate("/login");
+  //   console.log(1231)
+  // }
   // 获取所有页面的路径
   const arr = Base.map((item) => "/main/" + item.path);
-  console.log(arr);
+  React.useEffect(()=>{
+    if (Base.length === 1) {
+      navigate("/login");
+    }
+    // eslint-disable-next-line
+  },[])
   return (
     <div>
       <SilderBar roleRoute={Base} rolePath={arr} />
