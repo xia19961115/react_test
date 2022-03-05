124d798cd7212483ad5fa695acb952c79f10ed6d
夏宁 <478084629@qq.com>
2022-03-06 00:15:15 +0800

    1

--- src/pages/My/index.jsx
+++ src/pages/My/index.jsx
@@ -1,15 +1,21 @@
 /*
- * @Description: 
+ * @Description:
  * @Auther: xianing
  * @LastEditors: xianing
  * @Date: 2022-03-01 15:36:13
  * @LastEditTime: 2022-03-01 15:36:13
  */
-import React from 'react'
-
+import React from "react";
 const My = () => {
-  return (
-    <div>My</div>
-  )
-}
-export default My
+  const [num, setNum] = React.useState(0);
+  // React.useEffect(() => {
+  //   // console.log(num);
+  //   // eslint-disable-next-line
+  // }, [num]);
+  const handleClick = () => {
+    setNum(num + 1);
+    console.log(num)
+  };
+  return <div onClick={handleClick}>My</div>;
+};
+export default My;
